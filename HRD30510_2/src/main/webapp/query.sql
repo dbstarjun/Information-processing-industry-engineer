ed4d61d858d9038375452ccbe4be9c7d4c0035b1
ysjkdssj <127585771+dbstarjun@users.noreply.github.com>
2023-04-05 13:16:47 +0900

    .

---
+++ HRD30510_2/src/main/webapp/query.sql
@@ -0,0 +1,10 @@
+create table tbl_teacher_202201 (
+	teacher_code char(3),
+	class_name varchar2(12),
+	teacher_name varchar2(12),
+	class_price number(8),
+	teacher_regist_date char(8),
+	primary key(teacher_code)
+);
+	
+insert into tbl_teacher_202201 values('100', '김초'')
